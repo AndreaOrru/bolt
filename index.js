4151e5d0057b842411ff0b3591167e07f0b5bb22
Andrea Orru <andrea@orru.io>
2021-01-22 16:31:23 +0900

    Track all pending transactions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const Web3 = require('web3');
+require("dotenv").config();
+
+const Web3 = require("web3");
+const web3 = new Web3(
+  `wss://mainnet.infura.io/ws/v3/${process.env.PROJECT_ID}`
+);
+
+const subscription = web3.eth
+  .subscribe("pendingTransactions", (error, result) => {
+    if (!error) console.log(result);
+  })
+  .on("data", (transaction) => {
+    console.log(transaction);
+  });
