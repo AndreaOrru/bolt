69ef8222a52f1d8bc854f0f7a14e5f46fe309296
Andrea Orru <andrea@orru.io>
2021-02-06 00:52:38 +0900

    Listen to transactions to a certain address

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 require("dotenv").config();
 
-const Web3 = require("web3");
-const web3 = new Web3(
-  `wss://mainnet.infura.io/ws/v3/${process.env.PROJECT_ID}`
-);
+const WebSocket = require("ws");
+const ws = new WebSocket(process.env.WS_SERVER);
 
-const subscription = web3.eth
-  .subscribe("newBlockHeaders", (error, result) => {
-    if (!error) console.log(result);
-  })
-  .on("data", (blockHeader) => {
-    console.log(blockHeader);
-  });
+const simdjson = require('simdjson');
+
+function subscribeToPendingTransactions() {
+  ws.send(
+    JSON.stringify({
+      method: "parity_subscribe",
+      params: ["parity_pendingTransactions", []],
+      id: 1,
+      jsonrpc: "2.0",
+    })
+  );
+}
+
+ws.on("open", () => {
+  subscribeToPendingTransactions();
+});
+
+ws.on("message", (data) => {
+    const json = simdjson.parse(data);
+    if (json["method"] != 'parity_subscription') return;
+
+    const transactions = json["params"]["result"];
+    transactions.forEach(transaction => {
+        if (transaction["to"] == process.env.TARGET_ADDRESS) {
+            console.log(transaction);
+        }
+    });
+});
